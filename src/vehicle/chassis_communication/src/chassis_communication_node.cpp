79dea84c7ca8edc2069d02459df62ee1f4389618
SJ <slgsunjian@gmail.com>
2020-10-24 13:14:09 +0800

    use multi thread in one node, test well

--- src/vehicle/chassis_communication/src/chassis_communication_node.cpp
+++ src/vehicle/chassis_communication/src/chassis_communication_node.cpp
@@ -1,13 +1,18 @@
 #include "tcp_client/tcp_client.h"
+#include "chassis_communication_node.h"
 
-#include <ros/ros.h>
-#include <std_msgs/String.h>
+namespace smart_car
+{
+namespace chassis
+{
+
+const int kMultiThreadNum = 2;
 
 ::smart_car::TcpClient& tcp_client = ::smart_car::TcpClient::getSingleton();
 
-void controlMsgCallback(const std_msgs::String::ConstPtr& _msg)
+void ChassisCommunicationNode::transmitMsgCallback(const std_msgs::String::ConstPtr& _msg)
 {
-    printf("loop once \n");
+    printf("get control cmd\n");
     // get msg from topic and send to tcp server
     if ( ::smart_car::TcpClient::kSuccess != tcp_client.write2TcpServer(_msg->data.c_str(), _msg->data.size()) )
     {
@@ -15,69 +20,69 @@ void controlMsgCallback(const std_msgs::String::ConstPtr& _msg)
     }
 }
 
-void timerCallback(const ros::TimerEvent&)
+void ChassisCommunicationNode::receiveMsgCallback(const ros::TimerEvent&)
 {
-    ros::spinOnce();
+    printf("wait for chassis msg\n");
+    char rx_data[256];
+    // get msg from tcp server and send to topic
+    bzero(rx_data, sizeof(rx_data));
+    if ( ::smart_car::TcpClient::kSuccess == tcp_client.readFromTcpServer(rx_data, sizeof(rx_data)) )
+    {
+        printf("receive chassis msg\n");
+        std_msgs::String msg;
+        msg.data = rx_data;
+        pub_.publish(msg);
+    }
+    else
+    {
+        printf("ERROR reading from socket\n");
+    }
 }
 
-int main(int argc, char **argv)
+void ChassisCommunicationNode::init()
 {
     // init ros parameter
-    ros::init(argc, argv, "chassis_transmitter_node");
     ros::NodeHandle nh("~");
 
     std::string tcp_server_ip;
     if (! nh.getParam("tcp_server_ip", tcp_server_ip))
     {
         printf("no param named tcp_server_ip\n");
-        return -1;
     }
     int portno;
     if (! nh.getParam("portno", portno))
     {
         printf("no param named portno\n");
-        return -1;
     }
 
-    ros::Timer timer = nh.createTimer(ros::Duration(0.1), timerCallback);  // readFromTcpServer is blocked
-    ros::Subscriber sub = nh.subscribe("/chassis_control_cmd", 1000, controlMsgCallback);
-    ros::Publisher pub = nh.advertise<std_msgs::String>("chassis_signal", 1000);
+    timer_ = nh.createTimer(ros::Duration(0.1), &ChassisCommunicationNode::receiveMsgCallback, this);  // readFromTcpServer is blocked
+    pub_ = nh.advertise<std_msgs::String>("chassis_signal", 10);
+
+    sub_ = nh.subscribe("/chassis_control_cmd", 10, &ChassisCommunicationNode::transmitMsgCallback, this);
+
     ros::Rate try_connect_rate_hz(1);  // try connect every 1 second
-    ros::Rate check_callback_rate_hz(100);
 
-    char rx_data[256];
-
-    while (ros::ok())
+    // connect
+    while (!tcp_client.isConnected())
     {
-        // connect
-        while (!tcp_client.isConnected())
+        auto connect_status = tcp_client.connect2TcpServer(tcp_server_ip.c_str(), portno);
+        if (::smart_car::TcpClient::kSuccess == connect_status)
         {
-            auto connect_status = tcp_client.connect2TcpServer(tcp_server_ip.c_str(), portno);
-            if (::smart_car::TcpClient::kSuccess == connect_status)
-            {
-                printf("Connect successful\n");
-            }
-            else
-            {
-                printf("Connect failed, error code: %d\n", connect_status);
-                try_connect_rate_hz.sleep();
-            }
-        }
-
-        // get msg from tcp server and send to topic
-        bzero(rx_data, sizeof(rx_data));
-        if ( ::smart_car::TcpClient::kSuccess == tcp_client.readFromTcpServer(rx_data, sizeof(rx_data)) )
-        {
-            std_msgs::String msg;
-            msg.data = rx_data;
-            pub.publish(msg);
+            printf("Connect successful\n");
         }
         else
         {
-            printf("ERROR reading from socket");
+            printf("Connect failed, error code: %d\n", connect_status);
+            try_connect_rate_hz.sleep();
         }
-
-        // check_callback_rate_hz.sleep();
     }
-    return 0;
 }
+
+void ChassisCommunicationNode::run()
+{
+    ros::MultiThreadedSpinner spinner(kMultiThreadNum);
+    spinner.spin();
+}
+
+} // namespace chassis
+} // namespace smart_car
