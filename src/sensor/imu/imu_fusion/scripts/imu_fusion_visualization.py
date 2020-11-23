e19ec98ed62b050177978b790f28f67a8c0e0570
SJ_NANO <slgsunjian@gmail.com>
2020-11-23 17:14:13 +0800

    add fusion result vis using rviz tf

--- src/sensor/imu/imu_fusion/scripts/imu_fusion_visualization.py
+++ src/sensor/imu/imu_fusion/scripts/imu_fusion_visualization.py
@@ -2,22 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import rospy
-from visualization_msgs.msg import Marker
+import tf
 from sensor_msgs.msg import Imu
 
 class ImuFusionVisualization():
     def __init__(self):
-        self._pub_marker = rospy.Publisher('imu_fusion_vis', Marker, queue_size=1)
+        rospy.Subscriber('/sensor/imu/imu_fusion/fusion', Imu, self.imu_fusion_callback)
 
-        rospy.Subscriber('/sensor/imu/fusion', Imu, self.imu_fusion_callback)
-
-    def imu_fusion_callback(self, data):
-        marker = Marker()
-        marker.header.stamp = rospy.Time.now()
-        marker.header.frame_id = "ego_car"
-        marker.ns = 'imu'
-        marker.id = 0
-        marker.type = Cu
+    def imu_fusion_callback(self, msg):
+        br = tf.TransformBroadcaster()
+        br.sendTransform((0, 0, 0),
+                              (msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w),
+                              rospy.Time.now(),
+                              "ego_car",
+                              "map")
 
 if __name__ == "__main__":
     rospy.init_node('imu_fusion_vis_node')
