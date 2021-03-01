107667bcae23dc933ac66b5cc028023855f5300d
SJ <slgsunjian@163.com>
2021-03-01 21:26:01 +0800

    add little code

--- src/tool/rviz_plugin/color_depth_image_display/src/colorizer.cpp
+++ src/tool/rviz_plugin/color_depth_image_display/src/colorizer.cpp
@@ -11,6 +11,12 @@ Colorizer::~Colorizer()
 ::std::vector<uint8_t> Colorizer::process_frame(const ::std::vector<uint8_t>& _raw_depth_data
                                                 uint8_t _pixel_step)
 {
-    auto depth_data = reinterpret_cast<const uint16_t*>(_raw_depth_data.);
-    update_histogram()
+    // uint16
+    if(_pixel_step == 2)
+    {
+        auto depth_data = reinterpret_cast<const uint16_t*>(_raw_depth_data.);
+        update_histogram();
+    }
+    
+    
 }
